2afc4b1fed4b9432fcf2c3dd6ca76d8aba71409e
Watson Roberto de Oliveira Ferreira <watsonferreira@tce.ms>
2023-08-24 11:18:17 -0400

    criado o visual do header

--- front-end/src/components/header/Header.jsx
+++ front-end/src/components/header/Header.jsx
@@ -3,6 +3,12 @@ import logo from '../../assets/logo.png'
 import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { config } from '../../config'
+import IconUser from './icon_user/IconUser'
+import IconNotification from './icon_notification/IconNotification'
+import imgTest from "../../assets/card-user-test/anna.png"
+import IconMessage from './icon_notification/IconMessage'
+import BecomeUpgrade from './become_upgrade/BecomeUpgrade'
+import ButtonUpload from './button_upload/ButtonUpload'
 
 export default function Header({ setLoginOpen, session }) {
     return (
@@ -35,20 +41,36 @@ export default function Header({ setLoginOpen, session }) {
                 <div className="container-links">
                 {
                     !session.loggedIn && (<>
-                    <Link className="button" onClick={() => setLoginOpen(true)}>
-                        Entrar
-                    </Link>
+                        <Link className="button" onClick={() => setLoginOpen(true)}>
+                            Entrar
+                        </Link>
 
-                    <Link className="button" to="/register">
-                        Cadastrar-se
-                    </Link>
-                    </>) || (<>
-                    <Link className="button" onClick={async function() {
-                        await fetch(`${config.api}${config.endpoints.account.logout}`, {credentials: "include"});
-                        window.location.href = "/";
-                    }}>
-                        Sair
-                    </Link>
+                        <Link className="button" to="/register">
+                            Cadastrar-se
+                        </Link>
+                    </>) || 
+                    (<>
+                        <Link className="mx-3 text-decoration-none" to="/become-pro">
+                            <BecomeUpgrade></BecomeUpgrade>
+                        </Link>
+                        <Link className="px-3" to="/my-profile">
+                            <IconUser userImg={imgTest}></IconUser>
+                        </Link>
+                        <Link className="px-3" to="/message">
+                            <IconMessage notification={"teste"}></IconMessage>
+                        </Link>
+                        <Link className="px-3" to="/notifications">
+                            <IconNotification notification={"teste"}></IconNotification>
+                        </Link>
+                        <Link className="px-3" to="/upload">
+                            <ButtonUpload></ButtonUpload>
+                        </Link>
+                        <Link className="px-3 button" onClick={async function() {
+                            await fetch(`${config.api}${config.endpoints.account.logout}`, {credentials: "include"});
+                            window.location.href = "/";
+                        }}>
+                            Sair
+                        </Link>
                     </>)
                 }
                 </div>
---
+++ front-end/src/components/header/icon_user/IconUser.jsx
@@ -0,0 +1,5 @@
+export default function IconUser({userImg}) {
+    return (
+        <img src={userImg} className="rounded-circle" width={48} height={48} style={{boxShadow: "0px 3px 6px #00000029;"}}/>
+    )
+}
