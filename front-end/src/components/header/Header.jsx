11dbb06b266042be70da28b8d9e457117dd07761
Watson Roberto de Oliveira Ferreira <watsonferreira@tce.ms>
2023-08-29 12:12:21 -0400

    comitando estrutura do menu

--- front-end/src/components/header/Header.jsx
+++ front-end/src/components/header/Header.jsx
@@ -9,8 +9,10 @@ import imgTest from "../../assets/card-user-test/anna.png"
 import IconMessage from './icon_notification/IconMessage'
 import BecomeUpgrade from './become_upgrade/BecomeUpgrade'
 import ButtonUpload from './button_upload/ButtonUpload'
+import { useState } from 'react'
 
 export default function Header({ setLoginOpen, session }) {
+    const [toggleButton, useToggleButton] = useState(false)
     return (
         <div className="container-header">
             <div className="container-logo">
@@ -74,6 +76,28 @@ export default function Header({ setLoginOpen, session }) {
                     </>)
                 }
                 </div>
+            </div>
+            <div className="group-links-mobile">
+                <button className="border-0" type="button" onClick={() => {useToggleButton(!toggleButton)}}>
+                    <i>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
+                            <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/>
+                        </svg>
+                    </i>
+                </button>
+                {toggleButton && 
+                <div className='sidebar'>
+                    <div className='header'>
+                        <img src={logo} alt="Logo" />
+                        <button className="border-0" type="button" onClick={() => {useToggleButton(!toggleButton)}}>
+                            <i>
+                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
+                                    <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/>
+                                </svg>
+                            </i>
+                        </button>
+                    </div>
+                </div>}
             </div>
         </div>
     )
