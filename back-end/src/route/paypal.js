54b3577b2543c2b9a9545e2202878d337106b695
TidusBR <jeffersongimenez@gmail.com>
2023-09-26 14:17:07 -0300

    Add files via upload

---
+++ back-end/src/route/paypal.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import { captureOrder, createOrder } from '../paypal-api.js';
+import { DBConn } from '../database.js';
+
+export const PaypalRouter = express.Router();
+
+PaypalRouter.post("/create-order", async (req, res) => {
+    if (!req.session.loggedIn) {
+        return res.sendStatus(401);
+    }
+
+    try {
+        const { jsonResponse, httpStatusCode } = await createOrder(req.body);
+        res.status(httpStatusCode).json(jsonResponse);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error.message);
+    }
+});
+
+PaypalRouter.post("/capture-order", async (req, res) => {
+    if (!req.session.loggedIn) {
+        return res.sendStatus(401);
+    }
+
+    try {
+        const { orderID } = req.body;
+        const { jsonResponse, httpStatusCode } = await captureOrder(orderID);
+        
+        if (jsonResponse?.status === 'COMPLETED') {
+            // tempo atual em millis + (1 mês em millis) //  Date.now() + (730 * 60 * 60 * 1000);
+            const premiumTime =  Date.now() + 60000;
+
+            await DBConn.execute(`UPDATE accounts SET premium_level = 1, premium_time = ? WHERE id = ?;`, [
+                premiumTime,
+                req.session.accountInfo.id
+            ]);
+
+            req.session.accountInfo.premium_level = 1;
+            req.session.accountInfo.premium_time = premiumTime;
+            req.session.save();
+        }
+
+        res.status(httpStatusCode).json(jsonResponse);
+    } catch (error) {
+        console.error("Failed to capture order:", error);
+        res.status(500).json({ error: "Failed to capture order." });
+    }
+});
--- back-end/src/route/session.js
+++ back-end/src/route/session.js
@@ -3,9 +3,12 @@ import express from 'express'
 export const SessionRouter = express.Router();
 
 SessionRouter.post("/", async function(req, res) {
+    console.log(req.session.accountInfo);
+    
     res.json({loggedIn: req.session.loggedIn, account: {
         id: req.session.accountInfo?.id,
         name: req.session.accountInfo?.name,
-        profileName: req.session.accountInfo?.profileName
+        profileName: req.session.accountInfo?.profileName,
+        isPremium: req.session.accountInfo?.premium_level > 0
     }});
 });
