cedede7e485a54c62034f89d081182cdc8248c38
jvsleiman <sleimanjoaovitor@gmail.com>
2023-09-27 16:14:49 -0400

    adicionando partes restantes da página BecomePro

--- front-end/src/components/become_pro/BecomePro.jsx
+++ front-end/src/components/become_pro/BecomePro.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Button } from '@mui/material';
 import bannerImage from './become_pro_images/banner_becomepro.png';
 import foto_perfil from './become_pro_images/foto-perfil.png'
+import iphone_arq from './become_pro_images/iphone_arq.png'
+import styles from './BecomePro.css';
 
 const bannerImages = {
     width: '1920px',
@@ -48,20 +50,28 @@ const buttonPRO = {
 
 const profileProStyle = {
     marginTop: '40px',
-    marginLeft: '260px',
-  };
+    display: 'flex', // Adicione o display flex
+    flexDirection: 'row', // Altere de 'column' para 'row'
+    alignItems: 'center', // Alinhe verticalmente ao centro
+    justifyContent: 'center', // Centralize horizontalmente
+    width: '1430px',
+    maxWidth: '100%',
+    marginLeft: '15%',
+};
+
+const imgProfileStyle = {
+    marginRight: '144px', // Espaço entre a imagem e o texto
+};
 
 const textoProfile = {
-    transform: 'translate(-50%, -50%)',
-    position: 'absolute', 
-    left: '80%',
+    // Mantenha o estilo do texto como está
 }
 
 export default function BecomePro({ }) {
     return (
         <div>
             <div className="banner_container_pro" style={{ textAlign: 'center', position: 'relative' }}>
-                <img src={bannerImage} alt="Banner" style={bannerImages} />
+                <img src={bannerImage} alt="Banner" className='bannerImages' style={bannerImages} />
                 <h1 style={bannerTitulo}>
                     Tenha maior engajamento, se destaque!
                 </h1>
@@ -78,10 +88,23 @@ export default function BecomePro({ }) {
             </div>
 
             <div className='profile_pro' style={profileProStyle}>
-                <img src={foto_perfil} alt="foto-perfil" />
-                <div className='textoProfile'>
-                <h2>Saia na frente, tenha maior visibilidade.</h2>
-                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt.</p>
+                <div className='img_profile' style={imgProfileStyle}>
+                    <img src={foto_perfil} alt="foto-perfil" />
+                </div>
+                <div className='textoProfile' style={textoProfile}>
+                    <h2>Saia na frente, tenha maior visibilidade.</h2>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt.</p>
+                </div>
+            </div>
+
+
+            <div className='portifolio-container'>
+                <div className='portifolio-img'>
+                    <img src={iphone_arq} alt="" />
+                </div>
+                <div className='portifolio-text'>
+                    <h2>Expanda seu portifolio</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, amet. Cumque inventore quo sequi, debitis excepturi autem suscipit optio asperiores, eveniet amet quidem vel dolores enim. Reiciendis facere in alias.</p>
                 </div>
             </div>
         </div>
