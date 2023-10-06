c72fd810886c6f995647c96bdfccc8a497269570
WatsonROF <watson.shousuke@gmail.com>
2023-10-06 18:21:55 -0400

    nao tenho mais criatividade pra noemear commit

--- front-end/src/components/menu/InterestsForm.jsx
+++ front-end/src/components/menu/InterestsForm.jsx
@@ -7,7 +7,7 @@ import { useRef } from "react";
 export default function InterestsForm() {
 
     const [softwares, setSoftwares] = useState([]);
-    
+
     const [styles, setStyles] = useState(0);
 
     /**
@@ -28,8 +28,8 @@ export default function InterestsForm() {
 
     useEffect(() => {
         fetch(`${config.api}${config.endpoints.collection.upload_details}`, { credentials: "include" })
-        .then(response => response.json())
-        .then(data => setUploadDetails(JSON.parse(data)));
+            .then(response => response.json())
+            .then(data => setUploadDetails(JSON.parse(data)));
     }, [setUploadDetails]);
 
     // console.log(softwares);
@@ -38,7 +38,9 @@ export default function InterestsForm() {
     return <form className="row w-100">
         <div className="mb-4 col-12">
             <label className="form-label fw-bold">Softwares</label>
-            <select className="form-select" value="0" onChange={(e) => !softwares.includes(Number(e.target.selectedOptions[0].value)) && setSoftwares([...softwares, Number(e.target.selectedOptions[0].value)])}>
+            <select className="form-select" value="0" onChange={(e) => {
+                    !softwares.includes(Number(e.target.selectedOptions[0].value)) && setSoftwares([...softwares, Number(e.target.selectedOptions[0].value)])
+                }}>
                 <option value="" hidden></option>
                 {
                     uploadDetails.softwares.map(
@@ -50,25 +52,28 @@ export default function InterestsForm() {
             </select>
         </div>
         {
-        softwares.length > 0 &&
             <div className='col-12'>
                 {
-                    softwares.map((software_id, index) => (
-                        <span key={index} style={{border: "2px solid #EEEEEE", display: "inline-block"}} className='fw-bold p-2 rounded me-3 mb-3'>
-                            <div className="d-flex align-items-center">
-                                <i className='p-0 me-1' style={{display: "none"}}>
-                                    {/*AQUI VC RENDERIZA O SVG QUANDO O SOFTWARE ESTIVER COM ELE*/}
-                                    {uploadDetails.softwares.find(s => s.id === software_id)?.svg}
-                                </i>
-                                {uploadDetails.softwares.find(s => s.id === software_id)?.name} 
-                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512" className="ms-1" onClick={(e) => {
-                                    console.log(e.target.parentElement.parentElement);
-                                }}>
-                                    <path style={{pointerEvents: "none"}} d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>
-                                </svg>
-                            </div>
-                        </span>
-                    ))
+                    softwares.length > 0 &&
+                        softwares.map((software_id, index) => (
+
+                            <span key={index} id={software_id} style={{ border: "2px solid #EEEEEE", display: "inline-block" }} className='fw-bold p-2 rounded me-3 mb-3'>
+                                <div className="d-flex align-items-center">
+                                    <i className='p-0 me-1' style={{ display: "none" }}>
+                                        {/*AQUI VC RENDERIZA O SVG QUANDO O SOFTWARE ESTIVER COM ELE*/}
+                                        {uploadDetails.softwares.find(s => s.id === software_id)?.svg}
+                                    </i>
+                                    {uploadDetails.softwares.find(s => s.id === software_id)?.name} 
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512" className="ms-1" onClick={(e) => {
+                                        setSoftwares(softwares.splice(softwares.indexOf(Number(e.target.parentElement.parentElement.id)), 1))
+                                        console.log(softwares);
+                                    }}>
+                                        <path style={{ pointerEvents: "none" }} d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+                                    </svg>
+                                </div>
+                            </span>
+                        )
+                    )
                 }
             </div>
         }
