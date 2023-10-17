c1d23d9544dfe640784c9502f43a2a4cb729face
Watson Roberto de Oliveira Ferreira <watsonferreira@tce.ms>
2023-10-17 10:27:07 -0400

    button upload esta com material ui

--- front-end/src/components/header/button_upload/ButtonUpload.jsx
+++ front-end/src/components/header/button_upload/ButtonUpload.jsx
@@ -1,5 +1,7 @@
+import { Button } from '@mui/material';
+
 export default function ButtonUpload() {
-    return <button className="button-upload d-flex align-items-center py-1 px-3 bg-white rounded fw-bold" style={{border: "2px solid #EEEEEE", fontSize: "0.8rem"}}>
+    return <Button variant='outlined' className="button-upload d-flex align-items-center py-1 px-3 bg-white rounded fw-bold text-black" style={{border: "2px solid #EEEEEE", fontSize: "0.8rem"}}>
         <i>
             <svg xmlns="http://www.w3.org/2000/svg" id="ic_upload" width="24" height="24" viewBox="0 0 24 24">
                 <rect id="Rectangle_27" data-name="Rectangle 27" width="24" height="24" fill="none"/>
@@ -7,5 +9,5 @@ export default function ButtonUpload() {
             </svg>
         </i>
         Upload
-    </button>
+    </Button>
 }
